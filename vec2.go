edc795d0ba81fab8a72bcf2e7ef558679a819723
Alexander Johnston <Aliics@hotmail.com>
2025-02-04 22:28:26 +1300

    Docs and variable helpers.

--- vec2.go
+++ vec2.go
@@ -1,6 +1,9 @@
 package rlm
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+	"math"
+)
 
 type Vec2 rl.Vector2
 
@@ -14,7 +17,16 @@ var (
 )
 
 func NewVec2(x, y float32) Vec2 { return Vec2{X: x, Y: y} }
-func NewVec2N(v float32) Vec2   { return Vec2{X: v, Y: v} }
+func NewVec2N(v ...float32) Vec2 {
+	var result Vec2
+	if len(v) > 0 {
+		result.X = v[0]
+	}
+	if len(v) > 1 {
+		result.Y = v[1]
+	}
+	return result
+}
 
 func (v Vec2) XY() (x, y float32)     { return v.X, v.Y }
 func (v Vec2) YX() (y, x float32)     { return v.Y, v.X }
@@ -23,16 +35,38 @@ func (v Vec2) YY() (float32, float32) { return v.Y, v.Y }
 
 func (v Vec2) ToVec3(z float32) Vec3 { return Vec3{X: v.X, Y: v.Y, Z: z} }
 
-func (v Vec2) Add(v2 Vec2) Vec2         { return Vec2(rl.Vector2Add(rl.Vector2(v), rl.Vector2(v2))) }
-func (v Vec2) Subtract(v2 Vec2) Vec2    { return Vec2(rl.Vector2Subtract(rl.Vector2(v), rl.Vector2(v2))) }
-func (v Vec2) Multiply(v2 Vec2) Vec2    { return Vec2(rl.Vector2Multiply(rl.Vector2(v), rl.Vector2(v2))) }
-func (v Vec2) Divide(v2 Vec2) Vec2      { return Vec2(rl.Vector2Divide(rl.Vector2(v), rl.Vector2(v2))) }
-func (v Vec2) Max(v2 Vec2) Vec2         { return Vec2{X: max(v.X, v2.X), Y: max(v.Y, v2.Y)} }
-func (v Vec2) Min(v2 Vec2) Vec2         { return Vec2{X: min(v.X, v2.X), Y: min(v.Y, v2.Y)} }
+// Abs will create a new Vec2 where all axes are their absolute values.
+//
+//	(abs(x), abs(y))
+func (v Vec2) Abs() Vec2 {
+	return Vec2{
+		X: float32(math.Abs(float64(v.X))),
+		Y: float32(math.Abs(float64(v.Y))),
+	}
+}
+
+func (v Vec2) Add(v2 Vec2) Vec2      { return Vec2(rl.Vector2Add(rl.Vector2(v), rl.Vector2(v2))) }
+func (v Vec2) Subtract(v2 Vec2) Vec2 { return Vec2(rl.Vector2Subtract(rl.Vector2(v), rl.Vector2(v2))) }
+func (v Vec2) Multiply(v2 Vec2) Vec2 { return Vec2(rl.Vector2Multiply(rl.Vector2(v), rl.Vector2(v2))) }
+func (v Vec2) Divide(v2 Vec2) Vec2   { return Vec2(rl.Vector2Divide(rl.Vector2(v), rl.Vector2(v2))) }
+
+func (v Vec2) Max(v2 Vec2) Vec2 { return Vec2{X: max(v.X, v2.X), Y: max(v.Y, v2.Y)} }
+func (v Vec2) Min(v2 Vec2) Vec2 { return Vec2{X: min(v.X, v2.X), Y: min(v.Y, v2.Y)} }
+func (v Vec2) Clamp(min, max Vec2) Vec2 {
+	return Vec2(rl.Vector2Clamp(rl.Vector2(v), rl.Vector2(min), rl.Vector2(max)))
+}
+
 func (v Vec2) Dot(v2 Vec2) float32      { return rl.Vector2DotProduct(rl.Vector2(v), rl.Vector2(v2)) }
 func (v Vec2) Distance(v2 Vec2) float32 { return rl.Vector2Distance(rl.Vector2(v), rl.Vector2(v2)) }
-func (v Vec2) Scale(s float32) Vec2     { return Vec2(rl.Vector2Scale(rl.Vector2(v), s)) }
-func (v Vec2) Normalize() Vec2          { return Vec2(rl.Vector2Normalize(rl.Vector2(v))) }
+
+func (v Vec2) Scale(s float32) Vec2 { return Vec2(rl.Vector2Scale(rl.Vector2(v), s)) }
+
+func (v Vec2) Length() float32 { return rl.Vector2Length(rl.Vector2(v)) }
+func (v Vec2) Normalize() Vec2 { return Vec2(rl.Vector2Normalize(rl.Vector2(v))) }
+func (v Vec2) Negate() Vec2    { return Vec2(rl.Vector2Negate(rl.Vector2(v))) }
+
+func (v Vec2) Invert() Vec2 { return Vec2(rl.Vector2Invert(rl.Vector2(v))) }
+
 func (v Vec2) CrossProduct(v2 Vec2) float32 {
 	return rl.Vector2CrossProduct(rl.Vector2(v), rl.Vector2(v2))
 }
--- vec3.go
+++ vec3.go
@@ -1,25 +1,41 @@
 package rlm
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+	"math"
+)
 
 type Vec3 rl.Vector3
 
 var (
-	Vec3Zero  Vec3 = Vec3{}
-	Vec3XYZ   Vec3 = Vec3{X: 1, Y: 1, Z: 1}
-	Vec3XY    Vec3 = Vec3{X: 1, Y: 1}
-	Vec3YZ    Vec3 = Vec3{Y: 1, Z: 1}
-	Vec3XZ    Vec3 = Vec3{X: 1, Z: 1}
-	Vec3Up    Vec3 = Vec3{Y: 1}
-	Vec3Down  Vec3 = Vec3{Y: -1}
-	Vec3Left  Vec3 = Vec3{X: 1}
-	Vec3Right Vec3 = Vec3{X: -1}
-	Vec3Front Vec3 = Vec3{Z: 1}
-	Vec3Back  Vec3 = Vec3{Z: -1}
+	Vec3Zero     Vec3 = Vec3{}
+	Vec3Identity Vec3 = Vec3{}
+	Vec3XYZ      Vec3 = Vec3{X: 1, Y: 1, Z: 1}
+	Vec3XY       Vec3 = Vec3{X: 1, Y: 1}
+	Vec3YZ       Vec3 = Vec3{Y: 1, Z: 1}
+	Vec3XZ       Vec3 = Vec3{X: 1, Z: 1}
+	Vec3Up       Vec3 = Vec3{Y: 1}
+	Vec3Down     Vec3 = Vec3{Y: -1}
+	Vec3Left     Vec3 = Vec3{X: 1}
+	Vec3Right    Vec3 = Vec3{X: -1}
+	Vec3Front    Vec3 = Vec3{Z: 1}
+	Vec3Back     Vec3 = Vec3{Z: -1}
 )
 
 func NewVec3(x, y, z float32) Vec3 { return Vec3{X: x, Y: y, Z: z} }
-func NewVec3N(v float32) Vec3      { return Vec3{X: v, Y: v, Z: v} }
+func NewVec3N(v ...float32) Vec3 {
+	var result Vec3
+	if len(v) > 0 {
+		result.X = v[0]
+	}
+	if len(v) > 1 {
+		result.Y = v[1]
+	}
+	if len(v) > 2 {
+		result.Z = v[2]
+	}
+	return result
+}
 
 func (v Vec3) XXX() (float32, float32, float32)         { return v.X, v.X, v.X }
 func (v Vec3) XXY() (x1 float32, x2 float32, y float32) { return v.X, v.X, v.Y }
@@ -62,16 +78,75 @@ func (v Vec3) XX() (float32, float32) { return v.X, v.X }
 func (v Vec3) YY() (float32, float32) { return v.Y, v.Y }
 func (v Vec3) ZZ() (float32, float32) { return v.Z, v.Z }
 
-func (v Vec3) Add(v3 Vec3) Vec3         { return Vec3(rl.Vector3Add(rl.Vector3(v), rl.Vector3(v3))) }
-func (v Vec3) Subtract(v3 Vec3) Vec3    { return Vec3(rl.Vector3Subtract(rl.Vector3(v), rl.Vector3(v3))) }
-func (v Vec3) Multiply(v3 Vec3) Vec3    { return Vec3(rl.Vector3Multiply(rl.Vector3(v), rl.Vector3(v3))) }
-func (v Vec3) Divide(v3 Vec3) Vec3      { return Vec3(rl.Vector3Divide(rl.Vector3(v), rl.Vector3(v3))) }
-func (v Vec3) Max(v3 Vec3) Vec3         { return Vec3(rl.Vector3Max(rl.Vector3(v), rl.Vector3(v3))) }
-func (v Vec3) Min(v3 Vec3) Vec3         { return Vec3(rl.Vector3Min(rl.Vector3(v), rl.Vector3(v3))) }
+// Abs will create a new Vec3 where all axes are their absolute values.
+//
+//	(abs(x), abs(y), abs(z))
+func (v Vec3) Abs() Vec3 {
+	return Vec3{
+		X: float32(math.Abs(float64(v.X))),
+		Y: float32(math.Abs(float64(v.Y))),
+		Z: float32(math.Abs(float64(v.Z))),
+	}
+}
+
+// Add will add this Vec3's and another one's respective axes to form a third Vec3 where each axis is the sum of the
+// two Vec3's respective axes.
+//
+//	(x1+x2, y1+y2, z1+z2)
+func (v Vec3) Add(v3 Vec3) Vec3 { return Vec3(rl.Vector3Add(rl.Vector3(v), rl.Vector3(v3))) }
+
+// Subtract will subtract this Vec3's and another one's respective axes to form a third Vec3 where each axis is the
+// difference of the two Vec3's respective axes.
+//
+//	(x1-x2, y1-y2, z1-z2)
+func (v Vec3) Subtract(v3 Vec3) Vec3 { return Vec3(rl.Vector3Subtract(rl.Vector3(v), rl.Vector3(v3))) }
+
+// Multiply will multiply this Vec3's and another one's respective axes to form a third Vec3 where each axis is the
+// product of the two Vec3's respective axes.
+//
+//	(x1*x2, y1*y2, z1*z2)
+func (v Vec3) Multiply(v3 Vec3) Vec3 { return Vec3(rl.Vector3Multiply(rl.Vector3(v), rl.Vector3(v3))) }
+
+// Divide will divide this Vec3's and another one's respective axes to form a third Vec3 where each axis is the
+// product of the two Vec3's respective axes.
+//
+//	(x1/x2, y1/y2, z1/z2)
+func (v Vec3) Divide(v3 Vec3) Vec3 { return Vec3(rl.Vector3Divide(rl.Vector3(v), rl.Vector3(v3))) }
+
+// Max will determine the max for each of the axes.
+func (v Vec3) Max(v3 Vec3) Vec3 { return Vec3(rl.Vector3Max(rl.Vector3(v), rl.Vector3(v3))) }
+func (v Vec3) MaxN(vs ...Vec3) Vec3 {
+	result := v
+	for _, v3 := range vs {
+		result = v.Max(v3)
+	}
+	return result
+}
+
+// Min will determine the min for each of the axes.
+func (v Vec3) Min(v3 Vec3) Vec3 { return Vec3(rl.Vector3Min(rl.Vector3(v), rl.Vector3(v3))) }
+func (v Vec3) MinN(vs ...Vec3) Vec3 {
+	result := v
+	for _, v3 := range vs {
+		result = v.Min(v3)
+	}
+	return result
+}
+func (v Vec3) Clamp(min, max Vec3) Vec3 {
+	return Vec3(rl.Vector3Clamp(rl.Vector3(v), rl.Vector3(min), rl.Vector3(max)))
+}
+
 func (v Vec3) Distance(v3 Vec3) float32 { return rl.Vector3Distance(rl.Vector3(v), rl.Vector3(v3)) }
 func (v Vec3) Dot(v3 Vec3) float32      { return rl.Vector3DotProduct(rl.Vector3(v), rl.Vector3(v3)) }
-func (v Vec3) Scale(s float32) Vec3     { return Vec3(rl.Vector3Scale(rl.Vector3(v), s)) }
-func (v Vec3) Normalize() Vec3          { return Vec3(rl.Vector3Normalize(rl.Vector3(v))) }
+
+func (v Vec3) Scale(s float32) Vec3 { return Vec3(rl.Vector3Scale(rl.Vector3(v), s)) }
+
+func (v Vec3) Length() float32 { return rl.Vector3Length(rl.Vector3(v)) }
+
+func (v Vec3) Normalize() Vec3 { return Vec3(rl.Vector3Normalize(rl.Vector3(v))) }
+func (v Vec3) Negate() Vec3    { return Vec3(rl.Vector3Negate(rl.Vector3(v))) }
+func (v Vec3) Invert() Vec3    { return Vec3(rl.Vector3Invert(rl.Vector3(v))) }
+
 func (v Vec3) CrossProduct(v3 Vec3) Vec3 {
 	return Vec3(rl.Vector3CrossProduct(rl.Vector3(v), rl.Vector3(v3)))
 }
